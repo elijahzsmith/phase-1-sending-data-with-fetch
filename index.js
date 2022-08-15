544d5ab4ef7972a361380570f68312a7bf475272
Elijah Smith <elijahsmith.m@gmail.com>
2022-08-14 19:47:58 -0500

    commit tree 08098bd1b85f294c5c48e3e3823090ec66100013

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    return fetch("http://localhost:3000/users", {
+        method: "POST", // Post request to send data
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: `${userName}`,
+            email: `${userEmail}`
+        }),
+    }) .then(response => {
+        return response.json()
+    }) 
+    .then(data => document.body.append(data.id))
+    .catch(error => document.body.append(error.message)) 
+}
+
+
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// }) .then(response => {
+//     return response.json();
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log("ERROR")
+// );
+
